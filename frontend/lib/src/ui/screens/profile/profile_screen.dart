49778a87ba2ca92a1f73e13a51c61c65707f5d17
Irina <irina.bychkova06@mail.ru>
2025-07-14 20:46:31 +0300

    Improved UI

--- frontend/lib/src/ui/screens/profile/profile_screen.dart
+++ frontend/lib/src/ui/screens/profile/profile_screen.dart
@@ -1,24 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/src/ui/widgets/user_info_card.dart';
-import 'package:frontend/src/ui/widgets/progress_card.dart';
-import 'package:frontend/src/ui/widgets/daily_goal_card.dart';
-import 'package:frontend/src/ui/widgets/account_card.dart';
+import 'package:frontend/src/ui/widgets/profile_page/user_info_card.dart';
+import 'package:frontend/src/ui/widgets/profile_page/progress_card.dart';
+import 'package:frontend/src/ui/widgets/profile_page/daily_goal_card.dart';
+import 'package:frontend/src/ui/widgets/profile_page/account_card.dart';
+import 'package:frontend/src/models/registration_data.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+final demoUser = RegistrationData()
+  ..username = 'Ivan'
+  ..gender = 'Male'
+  ..birthDate = DateTime(2006, 11, 4)
+  ..height = 185
+  ..weight = 96 // start weight from registartion
+  ..goal = 'To lose weight'
+  ..goalWeight = 82;
+
+final double currentWeight = 90;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Profile'),
       ),
       body: SingleChildScrollView(
         child: Column(
-            children: const [
+            children: [
             SizedBox(height: 16),
-            UserInfoCard(),
-            ProgressCard(),
+            UserInfoCard(user: demoUser),
+            ProgressCard(user: demoUser, currentWeight: currentWeight),
             DailyGoalCard(),
             AccountCard(),
     ],
